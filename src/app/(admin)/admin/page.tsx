205e1bc08f9a16f77ad4cc1b9c1db82f6fe5521e
BitCoder <alejo_2986@hotmail.com>
2026-02-01 14:54:54 -0600

    refactor(admin): update admin dashboard with user info and logout
    [UI]: Update /admin/page to display current user details (username, name, role) and add a functional logout button.

--- src/app/(admin)/admin/page.tsx
+++ src/app/(admin)/admin/page.tsx
@@ -1,7 +1,30 @@
+"use client";
+import { useAuth } from "@/hooks/useAuth";
+import { Button } from "lambda-ui-components";
+
 export default function AdminPage() {
+    const { logout, isLoading, user } = useAuth();
     return (
         <div>
             <h1>Admin</h1>
+            <div>
+                <p>Usuario: {user?.username}</p>
+                <p>Nombre: {user?.name}</p>
+                <p>Rol: {user?.role}</p>
+                <p>ID: {user?.id}</p>
+                <p>Email: {user?.email}</p>
+            </div>
+
+            <Button
+                color="primary"
+                size="medium"
+                block
+                loading={isLoading}
+                disabled={isLoading}
+                loadingText="Cerrando sesión..."
+                label="Cerrar sesión"
+                onClick={() => logout()}
+            />
         </div>
     );
 }
