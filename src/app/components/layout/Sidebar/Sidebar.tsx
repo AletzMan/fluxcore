aa9ee821b3ee77dc933dd16917ff52e2e46c3182
BitCoder <alejo_2986@hotmail.com>
2026-02-02 12:07:10 -0600

    feat(sidebar): implement dynamic collapsible sidebar with logout

--- src/app/components/layout/Sidebar/Sidebar.tsx
+++ src/app/components/layout/Sidebar/Sidebar.tsx
@@ -1,19 +1,36 @@
-import { NavigationMenu, NavigationMenuData } from 'lambda-ui-components';
+import { Divider, NavigationMenuData } from 'lambda-ui-components';
 import styles from './Sidebar.module.scss'
+import Link from 'next/link';
+import { CloseSession } from './CloseSession';
+import { UserStar } from 'lucide-react';
+import { Icon } from '../../Icon';
 
 interface SidebarProps {
     menuData: NavigationMenuData[];
     currentPath: string;
+    title: string;
 }
 
-export const Sidebar = ({ menuData, currentPath }: SidebarProps) => {
+export const Sidebar = ({ menuData, currentPath, title }: SidebarProps) => {
     return (
         <div className={styles.sidebar}>
-            <NavigationMenu
-                data={menuData}
-                currentPath={currentPath}
-                size="large" showLines
-            />
+            <div>
+                <div>
+                    <Icon icon={UserStar} size={25} />
+                    <h3> {title}</h3>
+                </div>
+                <Divider />
+                {
+                    menuData.map((item) => (
+                        <Link
+                            className={`${styles.link} ${item.path === currentPath ? styles.active : ""}`}
+                            key={item.id}
+                            href={item.path || ""}
+                        > {item.icon} <span>{item.label}</span></Link>
+                    ))
+                }
+            </div>
+            <CloseSession />
         </div>
     );
 }
