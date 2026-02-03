2e09df7038a33b51e96924968712da51644e2aad
BitCoder <alejo_2986@hotmail.com>
2026-02-02 23:26:05 -0600

    feat(ui): implement AreaChart component with custom tooltip
    [UI]: Create a reusable AreaChart component using Recharts.
    [UI]: Implement ToolTipChart to provide detailed, formatted information on hover.
    [UI]: Add comprehensive SCSS module for chart layout, headers, and tooltip styling.

---
+++ src/app/components/ui/AreaChart/AreaChart.tsx
@@ -0,0 +1,109 @@
+"use client"
+import styles from './AreaChart.module.scss'
+import { AreaChart as RechartsAreaChart, Area, XAxis, YAxis, CartesianGrid, Tooltip, ResponsiveContainer } from 'recharts';
+import { ToolTipChart } from './ToolTipChart';
+import { formatCurrency, formatPercentage } from '../../../../lib/utils/common-utils';
+import { CurveType } from 'recharts/types/shape/Curve';
+
+
+export type AreaChartType = "porcentage" | "number" | "currency" | "decimal";
+
+interface AreaChartData {
+    name: string;
+    [key: string]: number | string;
+}
+
+export interface AreaChart {
+    data: AreaChartData[];
+    groups: string[];
+}
+
+interface AreaChartProps {
+    area: AreaChart;
+    type: AreaChartType;
+    title: string;
+    description: string;
+    curveType?: CurveType
+}
+
+export const AreaChart = ({ area, type, title, description, curveType }: AreaChartProps) => {
+
+    return (
+        <div className={styles.container}>
+            <header className={styles.container_header}>
+                <h2>{title}</h2>
+                <span>{description}</span>
+            </header>
+            <ResponsiveContainer >
+                <RechartsAreaChart
+                    className={styles.areachart}
+                    responsive
+                    data={area.data}
+                    margin={{
+                        top: 20,
+                        right: 0,
+                        left: 0,
+                        bottom: 0,
+                    }}
+                >
+                    <CartesianGrid
+                        strokeDasharray="3 3"
+                        stroke='var(--lambda-color-gray-600)'
+                    />
+                    <XAxis
+                        dataKey="name"
+                        stroke='var(--lambda-color-gray-600)'
+                        tick={{ fill: 'var(--foreground-title-color)', dy: 5 }}
+                    />
+                    <YAxis
+                        width="auto"
+                        tickFormatter={(value) => convertValue(value, type)}
+                        stroke='var(--lambda-color-gray-600)'
+                        tick={{ fill: 'var(--foreground-title-color)', dx: -5 }}
+                    />
+                    <Tooltip content={(props) => (
+                        <ToolTipChart {...props} type={type} active animationDuration={250} />
+                    )} />
+                    {area.groups.map((group, index) => (
+                        <Area
+                            key={index}
+                            animationEasing='ease-in-out'
+                            animationDuration={200}
+                            type={curveType}
+                            dataKey={group}
+                            stroke={colors[index]}
+                            strokeWidth={2}
+                            fill={colors[index]}
+                            fillOpacity={0.3} />
+                    ))}
+                </RechartsAreaChart>
+            </ResponsiveContainer>
+        </div>
+    );
+}
+
+const colors = [
+    "var(--lambda-color-cyan-500)",
+    "var(--lambda-color-green-500)",
+    "var(--lambda-color-yellow-500)",
+    "var(--lambda-color-violet-500)",
+    "var(--lambda-color-red-500)",
+    "var(--lambda-color-blue-500)",
+    "var(--lambda-color-pink-500)",
+    "var(--lambda-color-orange-500)",
+    "var(--lambda-color-brown-500)",
+];
+
+
+const convertValue = (value: number, type: "porcentage" | "number" | "currency" | "decimal") => {
+    switch (type) {
+        case "porcentage":
+            return formatPercentage(value).toString();
+        case "currency":
+            return formatCurrency(value).toString();
+        case "number":
+            return value.toString();
+        case "decimal":
+            return value.toString();
+    }
+}
---
+++ src/app/components/ui/AreaChart/ToolTipChart.tsx
@@ -0,0 +1,46 @@
+import { TooltipContentProps } from "recharts";
+import styles from "./AreaChart.module.scss";
+import { ValueType } from "recharts/types/component/DefaultTooltipContent";
+import { NameType } from "recharts/types/component/DefaultTooltipContent";
+import { formatPercentage, formatCurrency } from "../../../../lib/utils/common-utils";
+
+interface ToolTipChartProps<TValue extends ValueType, TName extends NameType>
+    extends TooltipContentProps<TValue, TName> {
+    type: "porcentage" | "number" | "currency" | "decimal";
+}
+
+export const ToolTipChart = <TValue extends ValueType, TName extends NameType>(
+    props: ToolTipChartProps<TValue, TName>
+) => {
+    const { active, payload, type } = props;
+    console.log(props);
+    if (active && payload && payload.length) {
+        const data = payload[0] && payload[0].payload;
+        const color = props.payload;
+        const total = payload.reduce((acc, item) => acc + item.value, 0);
+
+        return (
+            <div className={styles.tooltip}
+            >
+                <header className={styles.tooltip_header}>
+                    <h1  >{data.label}</h1>
+                </header>
+                <div className={styles.tooltip_values}>
+                    {payload.map((item, index) => (
+                        <span key={index} className={styles.tooltip_value}>
+                            <span className={styles.tooltip_tag} style={{ backgroundColor: color[index].stroke }}></span>
+                            <span>{item.name}: </span>
+                            <span>
+                                {type === "porcentage" && formatPercentage(item.value)}
+                                {type === "currency" && formatCurrency(item.value)}
+                                {type === "number" && item.value}
+                            </span>
+                        </span>
+                    ))}
+                </div>
+            </div>
+        );
+    }
+
+    return null;
+};
